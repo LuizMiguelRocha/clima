f05c753ffef5bb1272e7dfacce96f01c7b617594
Luiz Miguel <luizmiguel.ramos.escola@gmail.com>
2024-04-17 09:44:25 -0300

    cabra

---
+++ picles/resurce.cs
@@ -0,0 +1,19 @@
+namespace picles;
+
+public partial class resurce : ContentPage
+{
+    public class recursos
+    {
+        public int temp {get;set;}
+        public string deception {get;set;}
+        public int date {get;set;}    
+        public int time {get;set;}
+        public int condition_cod {get;set;}
+        public string currently {get;set;}
+        public string city {get;set;}
+        public int img_id {get;set;}
+        public int sunrise {get;set;}
+        public int sunset {get;set;}
+        public string city_name {get;set;}
+    }
+}
